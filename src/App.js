c431292c3492209d13eb7ac1c910649327128dd9
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2020-09-06 15:50:53 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { initReactI18next } from 'react-i18next';
 import './App.css';
 import ToDoList from './components/ToDoList';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <ToDoList/>
-    </div>
+    <Router>
+      <div className="App">
+        <table border="1">
+          <tbody>
+            <tr>
+              <td>
+                <Link to="/">Home</Link>
+              </td>
+              <td>
+                <Link to="/about">About</Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <hr></hr>
+        <Switch>
+          <Route path="/" exact>
+            <ToDoList />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
